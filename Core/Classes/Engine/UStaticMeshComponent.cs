6d357b362a1d7e6698823d08c00adc64002c2fbe
Martin N <na@na.no>
2022-06-09 11:44:38 +0200

    staticmeshcomponent structs start

--- Core/Classes/Engine/UStaticMeshComponent.cs
+++ Core/Classes/Engine/UStaticMeshComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Classes.Core;
+using Core.Classes.Core.Structs;
+using Core.Classes.Engine.Structs;
 using Core.Types;
 
 namespace Core.Classes.Engine;
@@ -10,4 +12,52 @@ public class UStaticMeshComponent : UPrimitiveComponent
         : base(name, @class, outer, ownerPackage, objectArchetype)
     {
     }
+
+    public List<FStaticMeshComponentLODInfo> FStaticMeshComponentLodInfos { get; set; }
+}
+
+public class FStaticMeshComponentLODInfo
+{
+    public List<UObject> ShadowMaps { get; set; } = new();
+    public List<UObject> ShadowVertexBuffers { get; set; } = new();
+    public FLightMap FLightMapRef { get; set; } = new();
+    public byte BLoadVertexColorData { get; set; }
+    public FColorVertexBuffer ColorVertexBuffer { get; set; } = new();
+}
+
+public class FColorVertexBuffer
+{
+    private uint NumVertices;
+
+    private uint Stride;
+    // TODO fill out the vertex data buffer when NumVertices > 0
+}
+
+public class FLightMap
+{
+    public enum LightMapType
+    {
+        None = 0,
+        LightMap1D = 1,
+        LightMap2D = 2
+    }
+
+    public List<FGuid> LightGuids { get; set; } = new();
+    public LightMapType Type { get; set; } = LightMapType.None;
+}
+
+public class FLightMap1D : FLightMap
+{
+    public UObject? ActorOwner { get; set; }
+    public FByteBulkData DirectionalSamples { get; set; } = new();
+    public FVector[] ScaleVectors { get; set; } = new FVector[3]; //Actually a FVector4 - but only x,y,z is serialized
+    public FByteBulkData SimpleSamples { get; set; } = new();
+}
+
+public class FLightMap2D : FLightMap
+{
+    public ULightMapTexture2D?[] Textures { get; set; } = new ULightMapTexture2D[3];
+    public FVector[] ScaleVectors { get; set; } = new FVector[3]; //Actually a FVector4 - but only x,y,z is serialized
+    public FVector2D CoordinateScale { get; set; }
+    public FVector2D CoordinateBias { get; set; }
 }
