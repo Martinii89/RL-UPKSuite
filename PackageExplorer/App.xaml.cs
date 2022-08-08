105c5995fc5458a18d14cf0d134efc830b634fbc
Martin N <m4rtini89@gmail.com>
2022-08-08 10:14:07 +0200

    license registration to get rid of that warning on every launch

--- PackageExplorer/App.xaml.cs
+++ PackageExplorer/App.xaml.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Syncfusion.Licensing;
 
-namespace PackageExplorer
+namespace PackageExplorer;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public App()
     {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            //.AddJsonFile("appsettings.json")
+            .AddUserSecrets<App>()
+            .Build();
+        var key = config["SyncfusionLiceneseKey"];
+        SyncfusionLicenseProvider.RegisterLicense(key);
     }
 }
