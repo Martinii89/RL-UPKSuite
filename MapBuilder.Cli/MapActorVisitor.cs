a615ffb6e9ffe948bc05689c058ff85e06f4881c
Martin Normann <m4rtini89@gmail.com>
2025-06-21 15:13:53 +0200

    actor visitor refactor

---
+++ MapBuilder.Cli/MapActorVisitor.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using RlUpk.Core.Classes.Core;
+using RlUpk.Core.Classes.Core.Structs;
+using RlUpk.Core.Classes.Engine;
+
+namespace RlUpk.MapBuilder.Cli;
+
+
+
+internal class MapActorVisitor
+{
+    
+    internal Dictionary<string, int> UnhandledTypes { get; } = new ();
+    
+    public IEnumerable<MeshComponentData> Visit(AActor actor)
+    {
+        var clz = actor.Class;
+        if (clz is null)
+        {
+            yield break;
+        }
+        if (actor.Class != null && actor.Class.IsA("StaticMeshCollectionActor"))
+        {
+            foreach (MeshComponentData meshComponentData in VisitMeshCollector(actor))
+            {
+                yield return meshComponentData;
+            }
+            yield break;
+        }
+
+        if (!UnhandledTypes.TryAdd(clz.Name, 1))
+        {
+            UnhandledTypes[clz.Name]++;
+        }
+    }
+    
+    private IEnumerable<MeshComponentData> VisitMeshCollector(AActor obj)
+    {
+        obj.Deserialize();
+        var meshComponents = obj.ScriptProperties
+            .Where(x => x.Name == "StaticMeshComponents")
+            .SelectMany(x => x.Value as List<object> ?? [])
+            .Cast<UStaticMeshComponent>();
+
+        foreach (var meshComponent in meshComponents)
+        {
+            meshComponent.Deserialize();
+            var hidden = GetScriptBool(meshComponent, "HiddenGame", false);
+            var staticMesh = GetScriptProperty<UStaticMesh?>(meshComponent, "StaticMesh", null);
+            if (staticMesh is null)
+            {
+                continue;
+            }
+            staticMesh.Deserialize();
+            
+            var staticMeshFullName = staticMesh.ToString();
+            var scale3d = GetScriptVector(meshComponent, "Scale3D", FVector.One);
+            var translation = GetScriptVector(meshComponent, "Translation", FVector.Zero);
+            var rotation = GetScriptRotator(meshComponent, "Rotation");
+            yield return new MeshComponentData(staticMeshFullName, hidden, scale3d, translation, rotation);
+        }
+    }
+    
+    
+    private bool TryFindScriptProperty<T>(UObject obj, string name, [MaybeNullWhen(false)] out T result)
+    {
+        var prop = obj.ScriptProperties.FirstOrDefault(x => x.Name == name);
+        if (prop?.Value is T propValue)
+        {
+            result = propValue;
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    private T GetScriptProperty<T>(UObject obj, string name, T defaultValue) => TryFindScriptProperty<T>(obj, name, out var result) ? result : defaultValue;
+
+    private FVector GetScriptVector(UObject obj, string name, FVector defaultValue)
+    {
+        FVector vec = new();
+        if (!TryFindScriptProperty<Dictionary<string, object>>(obj, name, out var result))
+        {
+            return new FVector()
+            {
+                X = defaultValue.X,
+                Y = defaultValue.Y,
+                Z = defaultValue.Z,
+            };
+        }
+
+        vec.X = result.GetValueOrDefault("X") as float? ?? defaultValue.X;
+        vec.Y = result.GetValueOrDefault("Y") as float? ?? defaultValue.Y;
+        vec.Z = result.GetValueOrDefault("Z") as float? ?? defaultValue.Z;
+        return vec;
+    }
+
+    private FRotator GetScriptRotator(UObject obj, string name)
+    {
+        FRotator vec = new();
+        if (!TryFindScriptProperty<Dictionary<string, object>>(obj, name, out var result))
+        {
+            return vec;
+        }
+
+        vec.Pitch = result.GetValueOrDefault("Pitch") as int? ?? 0;
+        vec.Roll = result.GetValueOrDefault("Roll") as int? ?? 0;
+        vec.Yaw = result.GetValueOrDefault("Yaw") as int? ?? 0;
+        return vec;
+    }
+    
+    private bool GetScriptBool(UObject obj, string name, bool defaultValue)
+    {
+        FRotator vec = new();
+        if (!TryFindScriptProperty<byte>(obj, name, out var result))
+        {
+            return defaultValue;
+        }
+
+        return result > 0;
+    }
+
+}
+
+internal record MeshComponentData(string StaticMeshName, bool Hidden, FVector Scale3d, FVector Translation, FRotator Rotation);
--- MapBuilder.Cli/MapBuilderProcessor.cs
+++ MapBuilder.Cli/MapBuilderProcessor.cs
@@ -16,7 +16,6 @@ namespace RlUpk.MapBuilder.Cli;
 
 internal class MapBuilderProcessor(PackageLoader packageLoader, PackageUnpacker unpacker)
 {
-
     public async Task ExportAssets(string file, string decryptedFolder, string assetFolder)
     {
         decryptedFolder = Path.GetFullPath(decryptedFolder);
@@ -35,10 +34,10 @@ internal class MapBuilderProcessor(PackageLoader packageLoader, PackageUnpacker 
         {
             return decryptedPath;
         }
-        
+
         await using var packageStream = File.OpenRead(file);
         await using var outputStream = File.OpenWrite(decryptedPath);
-        
+
         unpacker.Unpack(packageStream, outputStream);
         return decryptedPath;
     }
@@ -54,17 +53,21 @@ internal class MapBuilderProcessor(PackageLoader packageLoader, PackageUnpacker 
             return;
         }
 
-        var actors = FindObjectsOf("StaticMeshCollectionActor", package.ExportTable);
-        var meshComponentDatas = actors.SelectMany(VisitMeshCollector)
-            .Where(x => !x.Hidden)
-            // .Take(4)
-            .ToList();
+        var visitor = new MapActorVisitor();
+        var actors = package.ExportTable.Select(x => x.Object).OfType<AActor>();
+        var meshComponentDatas = actors.SelectMany(x => visitor.Visit(x)).ToList();
+
+        foreach (var unhandledType in visitor.UnhandledTypes.OrderBy(x => x.Value))
+        {
+            Console.WriteLine($"No visitor handler for: {unhandledType.Key}. count: {unhandledType.Value}");
+        }
+        
 
         var modelFinder = new ModelFinder(Path.GetFullPath(assetsFolder));
 
-        
+
         var merged = new SceneBuilder();
-        
+
         var models = meshComponentDatas.Select(x => modelFinder.LoadSceneBuilder(x.StaticMeshName)).ToList();
 
         for (int i = 0; i < models.Count; i++)
@@ -94,9 +97,8 @@ internal class MapBuilderProcessor(PackageLoader packageLoader, PackageUnpacker 
         string outputPath2 = Path.Combine(Path.GetDirectoryName(packageFile) ?? ".", $"{Path.GetFileNameWithoutExtension(packageFile)}_combined.json");
         using var jsonOut = File.CreateText(outputPath2);
         jsonOut.Write(JsonSerializer.Serialize(meshComponentDatas, new JsonSerializerOptions { WriteIndented = true }));
-        // Console.WriteLine(JsonSerializer.Serialize(meshComponentDatas, new JsonSerializerOptions { WriteIndented = true }));
     }
-    
+
     public Matrix4x4 CreateTransformMatrix(FVector translation, FRotator rotation, FVector scale)
     {
         float pitchRad = rotation.Pitch * (float)(Math.PI / 32768.0f);
@@ -108,100 +110,4 @@ internal class MapBuilderProcessor(PackageLoader packageLoader, PackageUnpacker 
         Matrix4x4 translationMatrix = Matrix4x4.CreateTranslation(translation.X, translation.Z, translation.Y);
         return scaleMatrix * rotationMatrix * translationMatrix;
     }
-
-    private static IEnumerable<UObject> FindObjectsOf(string name, ExportTable exportTable)
-    {
-        return exportTable.Where(x => x.Object?.Class?.IsA(name) ?? false).Select(x => x.Object!);
-    }
-
-    private IEnumerable<MeshComponentData> VisitMeshCollector(UObject obj)
-    {
-        obj.Deserialize();
-        var meshComponents = obj.ScriptProperties
-            .Where(x => x.Name == "StaticMeshComponents")
-            .SelectMany(x => x.Value as List<object> ?? [])
-            .Cast<UStaticMeshComponent>();
-
-        foreach (var meshComponent in meshComponents)
-        {
-            meshComponent.Deserialize();
-            var hidden = GetScriptBool(meshComponent, "HiddenGame", false);
-            var staticMesh = GetScriptProperty<UStaticMesh?>(meshComponent, "StaticMesh", null);
-            if (staticMesh is null)
-            {
-                continue;
-            }
-            staticMesh.Deserialize();
-            
-            
-            // var materials = GetScriptProperty<List<object>>(meshComponent, "Materials", []).Cast<UMaterialInterface>().ToList();
-            // materials.ForEach(x => x?.Deserialize());
-            var staticMeshFullName = staticMesh.ToString();
-            var scale3d = GetScriptVector(meshComponent, "Scale3D", FVector.One);
-            var translation = GetScriptVector(meshComponent, "Translation", FVector.Zero);
-            var rotation = GetScriptRotator(meshComponent, "Rotation");
-            yield return new MeshComponentData(staticMeshFullName, hidden, scale3d, translation, rotation);
-        }
-    }
-
-    private bool TryFindScriptProperty<T>(UObject obj, string name, [MaybeNullWhen(false)] out T result)
-    {
-        var prop = obj.ScriptProperties.FirstOrDefault(x => x.Name == name);
-        if (prop?.Value is T propValue)
-        {
-            result = propValue;
-            return true;
-        }
-
-        result = default;
-        return false;
-    }
-
-    private T GetScriptProperty<T>(UObject obj, string name, T defaultValue) => TryFindScriptProperty<T>(obj, name, out var result) ? result : defaultValue;
-
-    private FVector GetScriptVector(UObject obj, string name, FVector defaultValue)
-    {
-        FVector vec = new();
-        if (!TryFindScriptProperty<Dictionary<string, object>>(obj, name, out var result))
-        {
-            return new FVector()
-            {
-                X = defaultValue.X,
-                Y = defaultValue.Y,
-                Z = defaultValue.Z,
-            };
-        }
-
-        vec.X = result.GetValueOrDefault("X") as float? ?? defaultValue.X;
-        vec.Y = result.GetValueOrDefault("Y") as float? ?? defaultValue.Y;
-        vec.Z = result.GetValueOrDefault("Z") as float? ?? defaultValue.Z;
-        return vec;
-    }
-
-    private FRotator GetScriptRotator(UObject obj, string name)
-    {
-        FRotator vec = new();
-        if (!TryFindScriptProperty<Dictionary<string, object>>(obj, name, out var result))
-        {
-            return vec;
-        }
-
-        vec.Pitch = result.GetValueOrDefault("Pitch") as int? ?? 0;
-        vec.Roll = result.GetValueOrDefault("Roll") as int? ?? 0;
-        vec.Yaw = result.GetValueOrDefault("Yaw") as int? ?? 0;
-        return vec;
-    }
-    
-    private bool GetScriptBool(UObject obj, string name, bool defaultValue)
-    {
-        FRotator vec = new();
-        if (!TryFindScriptProperty<byte>(obj, name, out var result))
-        {
-            return defaultValue;
-        }
-
-        return result > 0;
-    }
 }
-
-internal record MeshComponentData(string StaticMeshName, bool Hidden, FVector Scale3d, FVector Translation, FRotator Rotation);
